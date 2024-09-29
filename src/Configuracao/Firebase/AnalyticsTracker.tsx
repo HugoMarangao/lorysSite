c7eeda0746d0bde4922ce0989bcb4ecbd4acfefe
HugoMarangao <hugomarangao@hotmail.com>
2024-09-29 15:04:19 +0200

    sistema de pixel

--- src/Configuracao/Firebase/AnalyticsTracker.tsx
+++ src/Configuracao/Firebase/AnalyticsTracker.tsx
@@ -1,8 +1,8 @@
-'use client';
+// src/Configuracao/Firebase/AnalyticsTracker.tsx
 
+'use client';
 import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
-import ReactPixel from 'react-facebook-pixel'; // Importando o pacote do Facebook Pixel
 import { analytics } from '@/Configuracao/Firebase/firebaseConf';
 import { logEvent } from 'firebase/analytics';
 import { db } from '@/Configuracao/Firebase/firebaseConf';
@@ -25,32 +25,24 @@ export default function AnalyticsTracker() {
   const [startTime, setStartTime] = useState<number>(Date.now());
 
   useEffect(() => {
-    // Inicializar o Facebook Pixel
-    ReactPixel.init('3064021873666359'); // Substitua pelo seu ID do Pixel
-    ReactPixel.pageView(); // Rastreia a visualização de página inicial
-
     const excludedRoutes = ['/dashboard', '/relatorio']; // Ajuste as rotas conforme necessário
-
     const logPageView = async (url: string) => {
       // Verificar se a rota está excluída
       if (excludedRoutes.some((route) => url.startsWith(route))) {
+        // Não registrar esta rota
         return;
       }
-
       if (analytics) {
         try {
           await logEvent(analytics, 'page_view', { page_path: url });
-          ReactPixel.pageView(); // Rastrear visualizações de página no Pixel
-
-          // Registro no Firestore
+          // Adiciona um documento ao Firestore
           await addDoc(collection(db, 'analytics'), {
             event_type: 'page_view',
             page_path: url,
             timestamp: new Date(),
             user_id: userId,
           });
-
-          // Atualizar dados de sessão do usuário no Firestore
+          // Atualizar dados de sessão do usuário
           const userSessionRef = doc(db, 'user_sessions', userId);
           await updateDoc(userSessionRef, {
             page_views: increment(1),
@@ -67,7 +59,6 @@ export default function AnalyticsTracker() {
               throw error;
             }
           });
-
           // Início de uma nova sessão
           if (!sessionStorage.getItem('session_started')) {
             sessionStorage.setItem('session_started', 'true');
@@ -86,11 +77,9 @@ export default function AnalyticsTracker() {
               }
             });
           }
-
           // Verifica se a página visualizada é uma página de produto
           if (url.startsWith('/produtos/')) {
             const productId = url.split('/produtos/')[1];
-
             // Atualizar estatísticas do produto
             const productStatsRef = doc(db, 'product_stats', productId);
             await updateDoc(productStatsRef, {
@@ -104,22 +93,17 @@ export default function AnalyticsTracker() {
                 throw error;
               }
             });
-
             // Atualizar produtos visualizados recentemente pelo usuário
             const userViewsRef = doc(db, 'user_views', userId);
             const userViewsSnap = await getDoc(userViewsRef);
-
             if (userViewsSnap.exists()) {
               const userViewsData = userViewsSnap.data();
               let recentViews: string[] = userViewsData.recentViews || [];
-
               recentViews = recentViews.filter((id) => id !== productId);
               recentViews.unshift(productId);
-
               if (recentViews.length > 10) {
                 recentViews = recentViews.slice(0, 10);
               }
-
               await updateDoc(userViewsRef, {
                 recentViews: recentViews,
               });
@@ -129,33 +113,25 @@ export default function AnalyticsTracker() {
                 recentViews: [productId],
               });
             }
-
             // Atualizar categorias visualizadas
             const productRef = doc(db, 'products', productId);
             const productSnap = await getDoc(productRef);
-
             if (productSnap.exists()) {
               const productData = productSnap.data();
               const categories = productData.selectedCategories || [];
-
               const userProfileRef = doc(db, 'user_profiles', userId);
               const userProfileSnap = await getDoc(userProfileRef);
-
               if (userProfileSnap.exists()) {
                 const updates: any = {};
-
                 categories.forEach((category: string) => {
                   updates[`categories_viewed.${category}`] = increment(1);
                 });
-
                 await updateDoc(userProfileRef, updates);
               } else {
                 const categories_viewed: any = {};
-
                 categories.forEach((category: string) => {
                   categories_viewed[category] = 1;
                 });
-
                 await setDoc(userProfileRef, {
                   user_id: userId,
                   categories_viewed,
@@ -170,7 +146,6 @@ export default function AnalyticsTracker() {
     };
 
     logPageView(pathname);
-
     // Capturar o tempo gasto na página quando o usuário sai ou muda de rota
     const handleRouteChange = () => {
       if (startTime && analytics) {
@@ -182,7 +157,6 @@ export default function AnalyticsTracker() {
             page_path: pathname,
             time_spent: timeSpent,
           });
-
           addDoc(collection(db, 'analytics'), {
             event_type: 'time_spent',
             page_path: pathname,
@@ -194,7 +168,6 @@ export default function AnalyticsTracker() {
       }
       setStartTime(Date.now());
     };
-
     window.addEventListener('beforeunload', handleRouteChange);
     return () => {
       window.removeEventListener('beforeunload', handleRouteChange);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { AuthProvider } from "@/Configuracao/Context/AuthContext";
 import { CartProvider } from "@/Configuracao/Context/CartContext";
 import AnalyticsTracker from "../Configuracao/Firebase/AnalyticsTracker"; // Importa o componente de rastreamento
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,12 +19,41 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
+      <head>
+        <Script
+          id="fb-pixel-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '537943838759167');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        {/* Código noscript para navegadores sem suporte a JavaScript */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=537943838759167&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
       <body className={inter.className}>
         <AuthProvider>
           <CartProvider>
             {children}
-            <AnalyticsTracker /> 
+            <AnalyticsTracker />
           </CartProvider>
         </AuthProvider>
       </body>
